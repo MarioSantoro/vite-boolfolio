2e393b55d7c610de89c8329741a1563459f77fd9
MarioSantoro <mariosantoro124@gmail.com>
2023-08-30 14:03:19 +0200

    create a new project and create a axios call for get api

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppMain from './components/AppMain.vue'
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    AppMain,
+  }
+}
+</script>
+
+<template>
+  <AppMain />
+</template>
+
+<style lang="scss">
+@use './assets/styles/generall.scss' as *;
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,27 @@
+<template></template>
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      apiUrl: 'http://127.0.0.1:8000/api/projects',
+    }
+  },
+
+  methods: {
+    getProjects() {
+      axios.get(this.apiUrl)
+        .then((response) => {
+          console.log(response.data.result.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  },
+  created() {
+    this.getProjects();
+  }
+}
+</script>
+<style lang="scss" scoped></style>
